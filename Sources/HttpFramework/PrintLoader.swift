1aeb7ed90ee3291a265580a888a47170fc53cbb1
Blueyds <9055449+blueyds@users.noreply.github.com>
2023-01-14 15:42:59 -0600

    fixed task loader

--- Sources/HttpFramework/PrintLoader.swift
+++ Sources/HttpFramework/PrintLoader.swift
@@ -3,6 +3,10 @@ import Foundation
 public class PrintLoader: HTTPLoader {
     public override func load(task: HTTPTask) {
         print("Loading \(task.request.url?.absoluteString)")
+		  task.addCompletionHandler(){result in
+			  print("Got result \(result)")
+			  completion(result)
+		  }
         super.load(task: task)
     }
     public override func load(request: HTTPRequest, completion: @escaping (HTTPResult) -> Void) {
