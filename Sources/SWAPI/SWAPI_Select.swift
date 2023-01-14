7b07135e1f8cd8caf09be62b9bc80ac5cc391654
Blueyds <9055449+blueyds@users.noreply.github.com>
2023-01-14 15:43:16 -0600

    added more people

--- Sources/SWAPI/SWAPI_Select.swift
+++ Sources/SWAPI/SWAPI_Select.swift
@@ -5,13 +5,13 @@ public enum SWAPISelectPerson: String {
     case c3po = "people/2"
     case r2d2 = "people/3"
     case darth = "people/4"
-    case five = "people/5"
-    case six = "people/6"
-    case seven = "people/7"
-    case eight = "people/8"
-    case nine = "people/9"
-    case ten = "people/10"
-    case eleven = "people/11"
+    case leia = "people/5"
+    case owen = "people/6"
+    case beru = "people/7"
+    case r5d4 = "people/8"
+    case biggs = "people/9"
+    case obiwan = "people/10"
+    case anakin = "people/11"
     case twelve = "people/12"
     case thirteen = "people/13"
     case fourteen = "people/14"
@@ -23,5 +23,4 @@ public enum SWAPISelectPerson: String {
     case twenty = "people/20"
     case twentyone = "people/21"
    // case twentytwo = "people/22"
-    
-}
+  
