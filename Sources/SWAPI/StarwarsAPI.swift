02db9d43017b34c9c66668b321bac98ddf894333
Blueyds <9055449+blueyds@users.noreply.github.com>
2023-01-16 09:36:39 -0600

    added debug info

--- Sources/SWAPI/StarwarsAPI.swift
+++ Sources/SWAPI/StarwarsAPI.swift
@@ -11,13 +11,14 @@ public class StarWarsAPI:JSONSimpleDecode {
         let env: HTTPLoader = ApplyEnvironment(environment: .swapi)
         let urlLoad: HTTPLoader = URLLoader()
         let printer: HTTPLoader = PrintLoader()
-		  let throttle: HTTPLoader = Throttle(maximumNumberOfRequests: 4)
-        self.loader = env --> throttle --> printer --> urlLoad
+		  let throttle: HTTPLoader = Throttle(maximumNumberOfRequests: 1)
+        self.loader = env --> throttle --> urlLoad
     }
     public func request(person: SWAPISelectPerson, completion: @escaping (SWAPI_Person) -> Void) {
         let request = HTTPRequest(path: person.rawValue)
 		  let task = HTTPTask(request: request){[self] result in
 			  if let body = result.response?.body {
+				  print("Decoding \(person.rawValue)")
 				  let d: SWAPI_Person = decode(from: body)
 				  completion(d)
 			  }
